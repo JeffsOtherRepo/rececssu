16acf29387c79bdf885401164a46cd0e12557919
JeffsOtherRepo <84904314+JeffsOtherRepo@users.noreply.github.com>
2024-02-14 02:23:46 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#home').attr('src', 'https://rececssu.wixsite.com/website');
+	//$('#home').attr('src', '');
+	
+	$('#proj').attr('src', 'https://rececssu.wixsite.com/website/description-du-projet');
+	//$('#proj').attr('src', '');
+	
+	$('#prog').attr('src', 'https://rececssu.wixsite.com/website/programme-de-travail');
+	//$('#prog').attr('src', '');
+	
+	$('#impact').attr('src', 'https://rececssu.wixsite.com/website/impacts-potentiels-du-projet');
+	//$('#impact').attr('src', '');
+	
+	$('#conf').attr('src', 'https://rececssu.wixsite.com/website/conf%C3%A9rences');
+	//$('#conf').attr('src', '');
+});
+
